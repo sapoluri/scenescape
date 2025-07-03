6a13ff5e9f53e3ba5046f759c18129a72fdcfb25
Tomasz Dorau <tomasz.dorau@intel.com>
2025-07-03 20:14:26 +0200

    Add LLA validation logic using assertions in geospatial functional test

--- tests/functional/tc_geospatial_ingest_publish.py
+++ tests/functional/tc_geospatial_ingest_publish.py
@@ -23,7 +23,7 @@ FRAMES_PER_SECOND = 10
 MAX_WAIT_TIMEOUT = 30
 LLA_VALUE = [50, 0, -6.37813751e+06]
 TRANSLATION_VALUE = [-0.3278, -0.3907, 0]
-BOUNDING_BOX = {'x': 0.1, 'y': 0.0, 'width': 0.24, 'height': 0.49}
+BOUNDING_BOX = {'x': 0.56, 'y': 0.0, 'width': 0.24, 'height': 0.49}
 # the following constants are used to verify that the LLA coordinates of the detected object are correct
 DETECTION_XYZ = [3.8679791719486474, 2.7517397452609087, 1.1225254457301852e-19]
 EXPECTED_DETECTION_LLA = [37.38688947231117, -121.96410520894621, 8.068826778282563]
@@ -77,8 +77,12 @@ class GeospatialIngestPublish(FunctionalTest):
       assert "translation" in object
       if self.outputLLA:
         assert "lat_long_alt" in object
-        if self.detectionValidator:
-          assert self.detectionValidator(object)
+        try:
+          if self.detectionValidator:
+            self.detectionValidator(object)
+        except AssertionError as e:
+          print(f"LLA validation failed: {e}")
+          raise
       else:
         assert "lat_long_alt" not in object
     return
@@ -188,7 +192,7 @@ class GeospatialIngestPublish(FunctionalTest):
     self.waitForUpdate(False)
     print("Enabling lat_long_alt output")
     self.rest.updateScene(self.sceneUID, {'output_lla': True})
-    self.detectionValidator = self._verifyLLA
+    self.detectionValidator = _verifyLLA
     self.waitForUpdate(True)
     print("Disabling lat_long_alt output")
     self.rest.updateScene(self.sceneUID, {'output_lla': False})
@@ -210,21 +214,6 @@ class GeospatialIngestPublish(FunctionalTest):
       self.recordTestResult()
     return
 
-  def _verifyLLA(self, detected_object):
-    '''
-    Verify the LLA coordinates of the detected object, assuming that:
-    - The LLA coordinates of four map corners are:
-      [[ 37.38685435, -121.96408120, 8.0], [ 37.38693520, -121.96408120, 8.0],
-      [ 37.38693520, -121.96413896, 8.0], [ 37.38685435, -121.96413896, 8.0]]
-      (the coordinates are loaded from the test database)
-    - The scene dimensions are:
-      x=0.24, y=0.49
-    - The translation of the detected object is:
-      [3.8679791719486474, 2.7517397452609087, 1.1225254457301852e-19]
-    '''
-    print(f"Verifying detected object LLA coordinates {detected_object['lat_long_alt']} vs expected {EXPECTED_DETECTION_LLA}")
-    return np.allclose(detected_object['lat_long_alt'], EXPECTED_DETECTION_LLA, rtol=1e-8)
-
   def check_geospatial_constants(self):
     """! Verify geospatial constants."""
     map_corners = [ [0, 0, 0],
@@ -233,8 +222,23 @@ class GeospatialIngestPublish(FunctionalTest):
           [0, MAP_RESOLUTION[1] / MAP_SCALE, 0] ]
     trs = calculateTRSLocal2LLAFromSurfacePoints(map_corners, MAP_CORNERS_LLA)
     lla = convertXYZToLLA(trs, DETECTION_XYZ)
-    assert np.allclose(lla, EXPECTED_DETECTION_LLA, rtol=1e-8), \
-      f"Constants verification failed! Expected LLA: {EXPECTED_DETECTION_LLA}, got: {lla}"
+    if not np.allclose(lla, EXPECTED_DETECTION_LLA, rtol=1e-8):
+      raise ValueError(f"Constants verification failed! Expected LLA: {EXPECTED_DETECTION_LLA}, got: {lla}")
+
+def _verifyLLA(detected_object):
+  '''
+  Verify the LLA coordinates of the detected object, assuming that:
+  - The LLA coordinates of four map corners are:
+    [[ 37.38685435, -121.96408120, 8.0], [ 37.38693520, -121.96408120, 8.0],
+    [ 37.38693520, -121.96413896, 8.0], [ 37.38685435, -121.96413896, 8.0]]
+    (the coordinates are loaded from the test database)
+  - The scene dimensions are:
+    x=0.24, y=0.49
+  - The translation of the detected object is:
+    [3.8679791719486474, 2.7517397452609087, 1.1225254457301852e-19]
+  '''
+  assert np.allclose(detected_object['lat_long_alt'], EXPECTED_DETECTION_LLA, rtol=1e-8), \
+    f"LLA verification failed! Expected LLA: {EXPECTED_DETECTION_LLA}, got: {detected_object['lat_long_alt']}"
 
 def test_geospatial_ingest_publish(request, record_xml_attribute):
   test = GeospatialIngestPublish(TEST_NAME, request, record_xml_attribute)
